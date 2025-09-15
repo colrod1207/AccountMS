fb7e12e783aadb58520cb1dc2807e1ff6dfe1aa0
Tu Nombre <tu_correo@loquesea.com>
2025-09-15 12:32:10 -0500

    listo

--- src/test/java/org/taller01/accountms/AccountMsApplicationTests.java
+++ src/test/java/org/taller01/accountms/AccountMsApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AccountMsApplicationTests {
 
-  @Test
-  void contextLoads() {}
+    @Test
+    void contextLoads() {}
 
 }
